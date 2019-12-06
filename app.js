fffed3f3fd885102b21ec668eb2d2dcf7f0e65c0
Matthew Reilly <matthewfreilly@gmail.com>
2019-12-06 02:57:48 -0600

    changed portfolio

--- app.js
+++ app.js
@@ -1,86 +1,86 @@
-let scene = document.querySelector('a-scene');
-let sky = document.querySelector('a-sky');
-let objectContainer = document.querySelector('#object-container');
+// let scene = document.querySelector('a-scene');
+// let sky = document.querySelector('a-sky');
+// let objectContainer = document.querySelector('#object-container');
 
-// random num generator
-function getRandomNumber(x, y) {
-  return Math.floor(Math.random() * x + y);
-}
+// // random num generator
+// function getRandomNumber(x, y) {
+//   return Math.floor(Math.random() * x + y);
+// }
 
-// get random hex color
-function getRandomColor() {
-  let letters = '0123456789abcdef';
-  let randomColor = '';
-  for (let i = 0; i < 8; i++) {
-    randomColor += letters[Math.floor(Math.random() * 6)];
-  }
-  return randomColor;
-}
+// // get random hex color
+// function getRandomColor() {
+//   let letters = '0123456789abcdef';
+//   let randomColor = '';
+//   for (let i = 0; i < 8; i++) {
+//     randomColor += letters[Math.floor(Math.random() * 6)];
+//   }
+//   return randomColor;
+// }
 
-// set sky values
-sky.setAttribute('color', `#${getRandomColor()}`);
-sky.setAttribute('animation__color', `property: color; dir: alternate; dur: 40000; easing: easeOutBack; loop: true; to: #${getRandomColor()}`);
+// // set sky values
+// sky.setAttribute('color', `#${getRandomColor()}`);
+// sky.setAttribute('animation__color', `property: color; dir: alternate; dur: 40000; easing: easeOutBack; loop: true; to: #${getRandomColor()}`);
 
-// change this value for more or less rings
-let totalRingElements = 15;
+// // change this value for more or less rings
+// let totalRingElements = 15;
 
-function generateAllElements() {
+// function generateAllElements() {
 
-  for(let a = 0; a < totalRingElements; a++){
+//   for(let a = 0; a < totalRingElements; a++){
 
-    // element params
-    let totalCircleElements = getRandomNumber(20, 5);
-    let elementScale = getRandomNumber(20, 5);
-    let scaleDuration = getRandomNumber(3000, 10000);
+//     // element params
+//     let totalCircleElements = getRandomNumber(20, 5);
+//     let elementScale = getRandomNumber(20, 5);
+//     let scaleDuration = getRandomNumber(3000, 10000);
 
-    // path params
-    let pathValOne = getRandomNumber(1, -10);
-    let pathValTwo = getRandomNumber(11, -20);
-    let pathDuration = getRandomNumber(6000, 10000);
+//     // path params
+//     let pathValOne = getRandomNumber(1, -10);
+//     let pathValTwo = getRandomNumber(11, -20);
+//     let pathDuration = getRandomNumber(6000, 10000);
 
-    for (let i = 1; i <= totalCircleElements; i++) {
+//     for (let i = 1; i <= totalCircleElements; i++) {
 
-      let currentRotation = 360 / totalCircleElements * i;
-      let rotateContainer = document.createElement('a-entity');
-      rotateContainer.setAttribute('rotation', `0 0 ${currentRotation}`);
+//       let currentRotation = 360 / totalCircleElements * i;
+//       let rotateContainer = document.createElement('a-entity');
+//       rotateContainer.setAttribute('rotation', `0 0 ${currentRotation}`);
 
-      // generate circle element and set params
-      let circleElementContainer = document.createElement('a-entity');
-      circleElementContainer.setAttribute('position', `0 1 0`);
-      let circleElement = document.createElement('a-entity');
-      circleElement.setAttribute('class', `circleElement`);
-      circleElement.setAttribute('scale', `${elementScale} ${elementScale} ${elementScale}`);
-      circleElement.setAttribute('material', `color:#${getRandomColor()}; metalness: 0; roughness: 0`);
-      circleElement.setAttribute('geometry', `primitive: sphere; radius: .5`);
-      circleElement.setAttribute('animation__yoyo', `property: scale; dir: alternate; dur: ${scaleDuration}; easing: easeOutBack; loop: true; to: 0 0 0`);
-      circleElementContainer.appendChild(circleElement);
-      rotateContainer.appendChild(circleElementContainer);
+//       // generate circle element and set params
+//       let circleElementContainer = document.createElement('a-entity');
+//       circleElementContainer.setAttribute('position', `0 1 0`);
+//       let circleElement = document.createElement('a-entity');
+//       circleElement.setAttribute('class', `circleElement`);
+//       circleElement.setAttribute('scale', `${elementScale} ${elementScale} ${elementScale}`);
+//       circleElement.setAttribute('material', `color:#${getRandomColor()}; metalness: 0; roughness: 0`);
+//       circleElement.setAttribute('geometry', `primitive: sphere; radius: .5`);
+//       circleElement.setAttribute('animation__yoyo', `property: scale; dir: alternate; dur: ${scaleDuration}; easing: easeOutBack; loop: true; to: 0 0 0`);
+//       circleElementContainer.appendChild(circleElement);
+//       rotateContainer.appendChild(circleElementContainer);
 
-      // generate path and apply it
-      let track1 = document.createElement('a-curve');
-      track1.setAttribute('class', `track${a}`);
-      scene.append(track1);
-      let point1 = document.createElement('a-curve-point');
-      point1.setAttribute('position', '0 0 0');
-      track1.append(point1);
-      let point2 = document.createElement('a-curve-point');
-      point2.setAttribute('position', `${pathValOne} ${pathValTwo} ${pathValOne}`);
-      track1.append(point2);
-      let point3 = document.createElement('a-curve-point');
-      point3.setAttribute('position', `${pathValTwo} ${pathValOne} ${pathValTwo}`);
-      track1.append(point3);
-      let point4 = document.createElement('a-curve-point');
-      point4.setAttribute('position', '0 0 0');
-      track1.append(point4);
-      circleElement.setAttribute(`alongpath`, `curve: .track${a}; dur: ${pathDuration}; loop: true`);
+//       // generate path and apply it
+//       let track1 = document.createElement('a-curve');
+//       track1.setAttribute('class', `track${a}`);
+//       scene.append(track1);
+//       let point1 = document.createElement('a-curve-point');
+//       point1.setAttribute('position', '0 0 0');
+//       track1.append(point1);
+//       let point2 = document.createElement('a-curve-point');
+//       point2.setAttribute('position', `${pathValOne} ${pathValTwo} ${pathValOne}`);
+//       track1.append(point2);
+//       let point3 = document.createElement('a-curve-point');
+//       point3.setAttribute('position', `${pathValTwo} ${pathValOne} ${pathValTwo}`);
+//       track1.append(point3);
+//       let point4 = document.createElement('a-curve-point');
+//       point4.setAttribute('position', '0 0 0');
+//       track1.append(point4);
+//       circleElement.setAttribute(`alongpath`, `curve: .track${a}; dur: ${pathDuration}; loop: true`);
 
-      // append element to main container
-      objectContainer.appendChild(rotateContainer);
+//       // append element to main container
+//       objectContainer.appendChild(rotateContainer);
 
-    }
+//     }
 
-  }
+//   }
 
-}
+// }
 
-generateAllElements()
+// generateAllElements()
